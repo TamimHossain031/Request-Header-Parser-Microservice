a0c845c086d685eff0d5e035ecbc5b144cbd8bf2
tamimSteadFast <steadfast.tamim@gmail.com>
2025-03-13 15:32:41 +0600

    add data

--- index.js
+++ index.js
@@ -20,8 +20,21 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip
+  const language = req.headers['accept-language']; // Client's preferred language
+  const software = req.headers['user-agent']; // Client's software/browser
+
+  // Send the response as JSON
+  res.json({
+    ipaddress,
+    language,
+    software,
+  });
+
+ 
+
+  
 });
 
 // listen for requests :)
